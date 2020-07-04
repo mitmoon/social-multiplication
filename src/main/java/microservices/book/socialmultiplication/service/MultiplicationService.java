d9e939900184e97d90af6caff3aef9cc9878c9f6
DESKTOP-PFBINLT\mitmoon <mitmoon@gmail.com>
2020-07-05 02:07:19 +0900

    P20 까지

---
+++ src/main/java/microservices/book/socialmultiplication/service/MultiplicationService.java
@@ -0,0 +1,7 @@
+package microservices.book.socialmultiplication.service;
+
+import microservices.book.socialmultiplication.domain.Multiplication;
+
+public interface MultiplicationService {
+    Multiplication createRandomMultiplication();
+}
---
+++ src/test/java/microservices/book/socialmultiplication/service/MultiplicationServiceTest.java
@@ -0,0 +1,33 @@
+package microservices.book.socialmultiplication.service;
+
+import microservices.book.socialmultiplication.domain.Multiplication;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class MultiplicationServiceTest {
+    @MockBean
+    private RandomGeneratorService randomGeneratorService;
+
+    @Autowired
+    private MultiplicationService multiplicationService;
+
+    @Test
+    public void createRandomMultiplicationTest() {
+        given (randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+
+        Multiplication multiplication = multiplicationService.createRandomMultiplication();
+
+        assertThat(multiplication.getFactorA()).isEqualTo(50);
+        assertThat(multiplication.getFactorB()).isEqualTo(30);
+        assertThat(multiplication.getResult()).isEqualTo(1500);
+    }
+}
