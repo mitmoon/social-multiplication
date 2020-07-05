cfb04deed1d49dd15da131d9724502e319aecfaf
DESKTOP-PFBINLT\mitmoon <mitmoon@gmail.com>
2020-07-06 01:24:07 +0900

    P29까지

---
+++ src/test/java/microservices/book/socialmultiplication/service/MultiplicatonServiceImplTest.java
@@ -0,0 +1,34 @@
+package microservices.book.socialmultiplication.service;
+
+import microservices.book.socialmultiplication.domain.Multiplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+public class MultiplicatonServiceImplTest {
+    private MultiplicationServiceImpl multiplicationServiceImpl;
+
+    @Mock
+    private RandomGeneratorService randomGeneratorService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService);
+    }
+
+    @Test
+    public void createRandomMultiplicationTest() {
+        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+
+        Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
+
+        assertThat(multiplication.getFactorA()).isEqualTo(50);
+        assertThat(multiplication.getFactorB()).isEqualTo(30);
+        assertThat(multiplication.getResult()).isEqualTo(1500);
+    }
+}
